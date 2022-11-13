5837c1a37e38ef2e7dcd8f8416ee0c4060d083b4
aSt0m <61126327+aSt0m@users.noreply.github.com>
2022-11-13 02:57:07 -0600

    Form,analitycs

--- js/script.js
+++ js/script.js
@@ -21,43 +21,64 @@ $(window).on("load", function() {
 
     // CONTACT FORM VALIDATION  
 
-    if($('#contact-form').length){
-      $('#submit').click(function(){
+    // if($('#contact-form').length){
+    //   $('#submit').click(function(){
         
-              var o = new Object();
-              var form = '#contact-form';
+    //           var o = new Object();
+    //           var form = '#contact-form';
         
-              var name = $('#contact-form .name').val();
-              var email = $('#contact-form .email').val();
-              // var phone = $('#contact-form .phone').val();
+    //           var name = $('#contact-form .name').val();
+    //           var email = $('#contact-form .email').val();
+    //           // var phone = $('#contact-form .phone').val();
         
-        if(name == '' || email == '')
-        {
-          $('#contact-form .response').html('<div class="failed">Please fill the required fields.</div>');
-          return false;
-        }
+    //     if(name == '' || email == '')
+    //     {
+    //       $('#contact-form .response').html('<div class="failed">Please fill the required fields.</div>');
+    //       return false;
+    //     }
               
-        $.ajax({
-            url:"sendemail.php",
-            method:"POST",
-            data: $(form).serialize(),
-            beforeSend:function(){
-                $('#contact-form .response').html('<div class="text-info"><img src="images/preloader.gif"> Loading...</div>');
-            },
-            success:function(data){
-                $('form').trigger("reset");
-                $('#contact-form .response').fadeIn().html(data);
-                setTimeout(function(){
-                    $('#contact-form .response').fadeOut("slow");
-                }, 5000);
-            },
-            error:function(){
-                $('#contact-form .response').fadeIn().html(data);
-            }
-        });
-    });
-    }
+    //     $.ajax({
+    //         url:"sendemail.php",
+    //         method:"POST",
+    //         data: $(form).serialize(),
+    //         beforeSend:function(){
+    //             $('#contact-form .response').html('<div class="text-info"><img src="images/preloader.gif"> Loading...</div>');
+    //         },
+    //         success:function(data){
+    //             $('form').trigger("reset");
+    //             $('#contact-form .response').fadeIn().html(data);
+    //             setTimeout(function(){
+    //                 $('#contact-form .response').fadeOut("slow");
+    //             }, 5000);
+    //         },
+    //         error:function(){
+    //             $('#contact-form .response').fadeIn().html(data);
+    //         }
+    //     });
+    // });
+    // }
 
 });
 
+// Contact form validation EmailJS
 
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Envian...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_qjx7jxe';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar';
+      alert('Mensaje enviado correctamente!');
+    }, (err) => {
+      btn.value = 'Enviar';
+      alert(JSON.stringify(err));
+    });
+});
